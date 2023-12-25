4bc1cd1f5e8d93e1a2f5e6d9903188a0b38e92bf
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2023-12-25 11:43:59 +0600

    Mochawesome report added

--- cypress/e2e/Tests/DashBoardPageTest.cy.js
+++ cypress/e2e/Tests/DashBoardPageTest.cy.js
@@ -2,7 +2,7 @@ import open_url from '../Base/OpenUrl'
 import login from '../Pages/LoginPage'
 import DashBoardPage from '../Pages/DashBoardPage'
 
-describe('Go to the PffIM', ()=>{
+describe('Go to the PIM', ()=>{
     //className objName = new className()
     const url = new open_url()
     const loginPage = new login()
